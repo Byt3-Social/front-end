e44b18e02884cc94c02a4cd712db57ebf9b5cf99
Leandro Pereira <leojgpereira@gmail.com>
2023-10-06 15:15:05 -0400

    Implementa tela de cadastro de organizações

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="header">
+        <div class="header__wrapper">
+            <i :class="icone"></i>
+            <h3 class="header__title">{{ titulo }}</h3>
+        </div>
+    </section>
+</template>
+
+<script>
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default {
+    name: 'Header',
+    props: ['titulo', 'icone']
+}
+</script>
+
+<style>
+.header {
+    background-color: #00145f;
+    color: #ffffff;
+    width: 100%;
+    height: 12rem;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 2rem 0;
+}
+
+.header__wrapper {
+    width: 100%;
+    max-width: 1100px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.header__wrapper .header__title {
+    margin: 0 0 0 1rem;
+}
+
+.bi.bi-building-fill {
+    font-size: 32px;
+}
+</style>
---
+++ src/views/prospeccao/CadastroOrganizacaoView.vue
@@ -0,0 +1,31 @@
+<template>
+    <Navbar></Navbar>
+    <main id="prospeccao">
+        <Header titulo="Cadastrar Organização" icone="bi bi-building-fill"></Header>
+        <FloatingPanel>
+            <template v-slot:FloatingPanelContent>
+                <FormularioCadastroOrganizacao></FormularioCadastroOrganizacao>
+            </template>
+        </FloatingPanel>
+    </main>
+    <FooterItem></FooterItem>
+</template>
+<script>
+import '../../assets/styles/cadastro-organizacao.css';
+import Navbar from '../../components/Navbar.vue';
+import FooterItem from '../../components/Footer.vue';
+import Header from '../../components/Header.vue';
+import FloatingPanel from '../../components/FloatingPanel.vue';
+import FormularioCadastroOrganizacao from '../../components/FormularioCadastroOrganizacao.vue';
+
+export default {
+    name: 'CadastroOrganizacaoView',
+    components: {
+        Navbar,
+        FooterItem,
+        Header,
+        FloatingPanel,
+        FormularioCadastroOrganizacao
+    }
+}
+</script>
