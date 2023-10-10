973c5ddade4f40d0b0d74147ff71eb3b94d3b92c
FernandaMascarenhas <fer.mascarenhass@gmail.com>
2023-10-10 19:08:00 -0400

    navegação entre tab feita

---
+++ src/components/compliance/FormGerenciarOrganizacoes.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="navbar__gerenciar-org">
+        <button class="botaoGerenciar"  :class="classeDadosGerais" @click="selecionaTab('DadosGerais')">Dados Gerais</button>
+        <button  class="botaoGerenciar" :class="classeDadosComplementares" @click="selecionaTab('DadosComplementares')">Dados Complementares</button>
+        <button class="botaoGerenciar" :class="classeDocumentos" @click="selecionaTab('Documentos')">Documentos</button>
+    </section>
+
+    <!-- Tab1 -->
+    <main v-if="exibirMainGerenciaOrg == 'DadosGerais'">
+        <DadosGerais></DadosGerais>
+    </main>
+
+    <!-- Tab2 -->
+    <main v-else-if="exibirMainGerenciaOrg == 'DadosComplementares'">
+        Dados Complementares
+    </main>
+
+    <!-- Tab3 -->
+    <main v-else-if="exibirMainGerenciaOrg == 'Documentos'">
+        Documentos
+    </main>
+
+</template>
+
+<script>
+import DadosGerais from './DadosGerais.vue';
+
+export default {
+    name: 'FormGerenciarOrganizacoes',
+    components: {
+        DadosGerais
+    },
+    data() {
+    return {
+        exibirMainGerenciaOrg: 'DadosGerais', // Tab inicial
+        classeDadosGerais: 'botaoGerenciar--Selecionado',
+        classeDadosComplementares: 'botaoGerenciar',
+        classeDocumentos: 'botaoGerenciar',
+        };
+    },
+    methods: {
+        selecionaTab: function(selecaoTab){
+            this.exibirMainGerenciaOrg = selecaoTab;
+            console.log(selecaoTab);
+
+            if(selecaoTab == 'DadosGerais') {
+                this.dadosGerais = 'botaoGerenciar--Selecionado';
+                this.dadosComplementares = 'botaoGerenciar';
+                this.documentos = 'botaoGerenciar';
+            }
+
+            else if(selecaoTab == 'DadosComplementares') {
+                this.dadosGerais = 'botaoGerenciar';
+                this.dadosComplementares = 'botaoGerenciar--Selecionado';
+                this.documentos = 'botaoGerenciar';
+            }
+
+            else if(selecaoTab == 'Documentos') {
+                this.dadosGerais = 'botaoGerenciar';
+                this.dadosComplementares = 'botaoGerenciar';
+                this.documentos = 'botaoGerenciar--Selecionado';
+            }
+        }
+    }
+}
+</script>
+
+<style>
+@import '../../assets/styles/gerenciar-cadastro.css'
+</style>
--- src/views/compliance/GerenciarCadastroView.vue
+++ src/views/compliance/GerenciarCadastroView.vue
@@ -1,36 +1,10 @@
-<template>
+<template> 
     <NavbarGerencia></NavbarGerencia>
     <main id="compliance">
         <Header titulo="Cadastro" icone="bi bi-file-earmark-fill"></Header>
         <FloatingPanel>
             <template v-slot:FloatingPanelContent>
-                <nav>
-                    <ul class="nav" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#dados-gerais" role="tab" aria-selected="true">Tab 1</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#dados-complementares" role="tab" aria-selected="false">Tab 2</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#documentos" role="tab" aria-selected="false">Tab 3</a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <DadosGerais></DadosGerais>
-            
-                 <!-- <div id="#dados-gerais" role="tabpanel" aria-hidden="true">
-                    <DadosGerais></DadosGerais>
-                </div> -->
-                <!--
-                <div id="dados-complementares" class="tab__content" role="tabpanel" aria-hidden="true">
-                    Conteúdo da Tab 2
-                </div>
-
-                <div id="documentos" class="tab__content" role="tabpanel" aria-hidden="true">
-                    Conteúdo da Tab 3
-                </div> -->
+                <FormGerenciarOrganizacoes></FormGerenciarOrganizacoes>
             </template>
         </FloatingPanel>
     </main>
@@ -42,24 +16,7 @@ import NavbarGerencia from '@/components/NavBarGerencia.vue';
 import Header from '@/components/Header.vue';
 import FloatingPanel from '@/components/FloatingPanel.vue';
 import Footer from '@/components/Footer.vue';
-import DadosGerais from '@/components/compliance/DadosGerais.vue'
-
-const navLinks = document.querySelectorAll('.nav-link')
-navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const tabId = link.getAttribute('href').replace('#', '');
-        const tabContents = document.querySelectorAll('.tab-content');
-        tabContents.forEach(tab => {
-            tab.setAttribute('aria-hidden', 'true');
-        });
-        document.getElementById(tabId).setAttribute('aria-hidden', 'false');
-        navLinks.forEach(navLink => {
-            navLink.setAttribute('aria-selected', 'false');
-        });
-        link.setAttribute('aria-selected', 'true');
-    });
-});
+import FormGerenciarOrganizacoes from '@/components/compliance/FormGerenciarOrganizacoes.vue';
 
 export default {
     name: 'GerenciarCadastro',
@@ -68,7 +25,7 @@ export default {
         Header,
         FloatingPanel,
         Footer,
-        DadosGerais
+        FormGerenciarOrganizacoes
     }
 }
 </script>
